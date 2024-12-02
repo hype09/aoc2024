c7e20adddeb2e17b6b849015ca127893eadd03ba
Paik Paustian <paik.paustian@gmail.com>
2024-12-02 22:49:17 +0100

    simplify even more

--- Day01/Part02.fs
+++ Day01/Part02.fs
@@ -3,6 +3,5 @@ module Day01.Part02
 let solution =
     let incr = Option.map ((+) 1) >> Option.defaultValue 1 >> Some
     System.IO.File.ReadLines(__SOURCE_DIRECTORY__ + "/input02.txt")
-    |> Seq.map (_.Split("   ") >> fun xs -> int xs.[0], int xs.[1])
-    |> Seq.fold (fun (seq, m) (a, b) -> (seq @ [ a ], Map.change b incr m)) (List.empty, Map.empty)
+    |> Seq.fold (fun (seq, m) s -> s.Split("   ") |> fun xs -> (int xs.[0] :: seq, Map.change (int xs.[1]) incr m)) ([], Map.empty)
     |> fun (xs, m) -> Seq.map (fun a -> (m.TryFind a) |> Option.defaultValue 0 |> (*) a) xs |> Seq.sum
