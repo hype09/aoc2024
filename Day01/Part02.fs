53f8a334ad07945edbe1edc92679b2c37e229dbd
Paik Paustian <paik.paustian@gmail.com>
2024-12-02 22:36:31 +0100

    "simplify" day 01 / 2

--- Day01/Part02.fs
+++ Day01/Part02.fs
@@ -1,18 +1,8 @@
 module Day01.Part02
 
-let splitInputLine (line: string) = line.Split("   ")
-let arrToTuple (arr: 'a array) = arr.[0], arr.[1]
-let castTupleToInt (a, b) = int a, int b
-
-let countOccurrences seq =
-    let incr i = Some (i |> Option.defaultValue 0 |> (+) 1)
-    Seq.fold (fun acc a -> Map.change a incr acc) Map.empty seq
-
-let calcDistance a b = b |> Option.defaultValue 0 |> (*) a
-    
 let solution =
+    let incr = Option.map ((+) 1) >> Option.defaultValue 1 >> Some
     System.IO.File.ReadLines(__SOURCE_DIRECTORY__ + "/input02.txt")
-    |> Seq.map (splitInputLine >> arrToTuple >> castTupleToInt)
-    |> fun seq ->
-        let m = seq |> Seq.map snd |> countOccurrences
-        seq |> Seq.map (fst >> (fun a -> calcDistance a (m.TryFind a))) |> Seq.sum
+    |> Seq.map (_.Split("   ") >> fun xs -> int xs.[0], int xs.[1])
+    |> Seq.fold (fun (seq, m) (a, b) -> (seq @ [ a ], Map.change b incr m)) (List.empty, Map.empty)
+    |> fun (xs, m) -> Seq.map (fun a -> (m.TryFind a) |> Option.defaultValue 0 |> (*) a) xs |> Seq.sum
