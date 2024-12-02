09eb4a65136e7ab7a3f276577db6c034ce4a6939
Paik Paustian <paik.paustian@gmail.com>
2024-12-02 07:55:23 +0100

    Made code a bit different

--- Day01/Part01.fs
+++ Day01/Part01.fs
@@ -1,12 +1,15 @@
 module Day01.Part01
 
+let splitInputLine (line: string) = line.Split("   ")
+let arrToTuple (arr: 'a array) = arr.[0], arr.[1]
+let castTupleToInt (a, b) = int a, int b
+let calcDistance (a, b) = abs (a - b)
+
 let solution =
     System.IO.File.ReadLines(__SOURCE_DIRECTORY__ + "/input01.txt")
-    |> Seq.map (fun line -> line.Split("   "))
-    |> Seq.map (fun arr -> int arr.[0], int arr.[1])
-    |> Seq.toList
-    |> List.unzip
-    |> fun (l1, l2) -> (List.sort l1, List.sort l2)
-    |> fun (l1, l2) -> List.zip l1 l2
-    |> List.map (fun (a, b) -> abs (a - b))
-    |> List.sum
+    |> Seq.map (splitInputLine >> arrToTuple >> castTupleToInt)
+    |> fun (seq) -> seq |> Seq.map fst, seq |> Seq.map snd
+    |> fun (l1, l2) -> (Seq.sort l1, Seq.sort l2)
+    ||> Seq.zip
+    |> Seq.map calcDistance
+    |> Seq.sum
