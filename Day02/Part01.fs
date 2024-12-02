dd7f49616dae4ca3c99db93bdab8e277e45f1190
Paik Paustian <paik.paustian@gmail.com>
2024-12-02 09:55:34 +0100

    made solution non-recursive

--- Day02/Part01.fs
+++ Day02/Part01.fs
@@ -2,22 +2,12 @@ module Day02.Part01
 
 let lineToIntArray (line: string) = line.Split(" ") |> Array.map int
 
-let rec isAsc arr =
-    if Array.length arr < 2 then true
-    else arr.[0] < arr.[1] && isAsc arr[1..]
-        
-let rec isDesc arr =
-    if Array.length arr < 2 then true
-    else arr.[0] > arr.[1] && isDesc arr[1..]
+let isAsc arr = arr |> Seq.windowed 2 |> Seq.forall (fun [|a;b|] -> a < b)
+let isDesc arr = arr |> Seq.windowed 2 |> Seq.forall (fun [|a;b|] -> a > b)
+let isIntervalSafe arr = arr |> Seq.windowed 2 |> Seq.forall (fun [|a;b|] -> abs(a - b) <= 3)
     
-let rec isInSafeInterval arr =
-    if Array.length arr < 2 then true
-    else
-        let dist = abs(arr.[0] - arr.[1])
-        dist <= 3 && isInSafeInterval arr[1..]
-
 let solution =
     System.IO.File.ReadLines(__SOURCE_DIRECTORY__ + "/input01.txt")
     |> Seq.map lineToIntArray
-    |> Seq.filter (fun arr -> isInSafeInterval arr && (isAsc arr || isDesc arr))
+    |> Seq.filter (fun arr -> isIntervalSafe arr && (isAsc arr || isDesc arr))
     |> Seq.length
