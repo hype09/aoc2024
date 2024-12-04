4f19e959cc2b0398ae8a98357d63718c7b2cbfe6
Paik Paustian <paik.paustian@gmail.com>
2024-12-04 20:34:19 +0100

    solved 4-2

--- Day04/Part02.fs
+++ Day04/Part02.fs
@@ -1,7 +1,16 @@
-module Day04.Part04
+module Day04.Part02
 
 open System.Text.RegularExpressions
 
 let solution =
     System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + "/input01.txt")
-    |> Seq.windowed 3
+    |> Array.map Seq.toArray
+    |> Array.windowed 3
+    |> Array.map (fun xs -> Array.zip3 xs[0] xs[1] xs[2])
+    |> Array.map (fun xs ->
+        xs
+        |> Array.windowed 3
+        |> Array.map ((Array.collect (fun (a, b, c) -> [| a; b; c |])) >> System.String)
+        |> Array.filter (fun s -> Regex.IsMatch(s, "M.M.A.S.S|M.S.A.M.S|S.M.A.S.M|S.S.A.M.M"))
+        |> Array.length)
+    |> Array.sum
