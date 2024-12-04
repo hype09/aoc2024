1b7d33aedac17eeb581bb49efcf7098d6718c6b3
Paik Paustian <paik.paustian@gmail.com>
2024-12-04 09:57:19 +0100

    simplify solution for 4-1

--- Day04/Part01.fs
+++ Day04/Part01.fs
@@ -3,14 +3,8 @@ module Day04.Part01
 open System.Text.RegularExpressions
 
 let solution =
-    let transpose (xs: string array) =
-        xs
-        |> Seq.map Seq.toList
-        |> Seq.toList
-        |> List.transpose
-        |> Seq.map (fun chars -> System.String(List.toArray chars))
-        |> Seq.toArray
-    let diagonalise (xs: string array) =
+    let trans = Array.map Seq.toArray >> Array.transpose >> Array.map System.String
+    let diag (xs: string array) =
         let height = xs.Length
         let width = xs[0].Length
         [ for i in 0..(width + height - 2) do
@@ -19,14 +13,8 @@ let solution =
             let k = i - j
             if k < height && j < width then yield xs[k][j]                              
           ] |> List.toArray |> System.String 
-        ]
-    let countXmasInLine x = Regex.Matches(x, "XMAS").Count + Regex.Matches(x, "SAMX").Count 
-    let countXmasInLines xs = Seq.map countXmasInLine xs |> Seq.sum
-    let countHorizontal xs = countXmasInLines xs
-    let countVertical xs = countXmasInLines (transpose xs)
-    let countMainDiagonal xs = countXmasInLines (diagonalise xs)
-    let countSecondaryDiagonal xs = countXmasInLines (diagonalise (Array.rev xs))
-        
-    let xs = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + "/input01.txt")
-    countHorizontal xs + countVertical xs + countMainDiagonal xs + countSecondaryDiagonal xs
-    
+        ] |> List.toArray
+    System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + "/input01.txt")
+        |> fun xs -> Array.concat [for t in [id; trans; diag; Array.rev >> diag] do t xs]
+        |> Seq.map (fun x -> Regex.Matches(x, "XMAS").Count + Regex.Matches(x, "SAMX").Count)
+        |> Seq.sum
