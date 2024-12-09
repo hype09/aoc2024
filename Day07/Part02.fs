e3058c927d6cee6a65c9f21d2d74dd2022880c68
Paik Paustian <paik.paustian@gmail.com>
2024-12-09 21:17:52 +0100

    Solved 7-2

--- Day07/Part02.fs
+++ Day07/Part02.fs
@@ -1,3 +1,21 @@
 module Day07.Part02
 
-let solution = 1
+open System;
+
+let parseLine (l: string) =
+    l.Split(':')
+    |> fun ls -> (uint64 ls[0], ls[1].Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Array.map uint64 |> Array.toList)
+
+let concat (x: uint64) (y: uint64) = uint64 ((string x) + (string y))
+
+let rec validate (target: uint64) (res: uint64) (vals: uint64 list) =
+    match vals with
+    | [v] -> target = (res + v) || target = (res * v) || target = (concat res v)
+    | v::vs -> validate target (res + v) vs || validate target (res * v) vs || validate target (concat res v) vs
+
+let solution =
+    System.IO.File.ReadLines(__SOURCE_DIRECTORY__ + "/input01.txt")
+    |> Seq.map parseLine
+    |> Seq.filter (fun (res, v::vs) -> validate res v vs)
+    |> Seq.map fst
+    |> Seq.sum
