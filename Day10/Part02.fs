63ddb70ae60acb6c489fa1d76b8d4ae2585f3741
Paik Paustian <paik.paustian@gmail.com>
2024-12-10 10:26:50 +0100

    Solved 10-2

--- Day10/Part02.fs
+++ Day10/Part02.fs
@@ -1,3 +1,31 @@
 module Day10.Part02
 
-let solution = 1
+type Map = int array array
+
+let trailheads (m: Map) =
+    Array.mapi (fun y row ->
+        row
+        |> Array.mapi (fun x cell -> if cell = 0 then Some (x, y) else None)
+        |> Array.choose id
+    ) m
+    |> Array.concat
+    
+let findNextCells (m: Map) (l: int) (x: int, y: int) =
+    [for x', y' in [(0, 1); (0, -1); (1, 0); (-1, 0)] do yield (x + x', y + y')]
+    |> Seq.filter (fun (x, y) -> x >= 0 && x < m[0].Length && y >= 0 && y < m.Length)
+    |> Seq.filter (fun (x, y) -> m[y].[x] = l)
+    
+let rec countTrails (m: Map) (tx: int, ty: int)=
+    match m[ty].[tx] with
+    | 9 -> 1
+    | l ->
+           findNextCells m (l + 1) (tx, ty)
+           |> Seq.map (countTrails m)
+           |> Seq.sum
+
+let solution =
+    System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + "/input01.txt")
+    |> Array.map (Seq.toArray >> Array.map (string >> int))
+    |> fun m -> m, trailheads m
+    |> fun (m, xs) -> Seq.map (countTrails m) xs
+    |> Seq.sum
